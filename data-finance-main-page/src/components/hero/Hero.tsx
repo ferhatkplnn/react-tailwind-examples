030b7eec6d957ca1a318bc073c7fa49284bdc584
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-17 00:07:24 +0300

    feat: Implement Hero component for landing page header
    - Created a new component called Hero for the header section of the landing page
    - Designed layout with responsive typography and styling
    - Integrated Typed component for dynamic text display in the header subtitle

--- data-finance-main-page/src/components/hero/Hero.tsx
+++ data-finance-main-page/src/components/hero/Hero.tsx
@@ -1,3 +1,24 @@
-export const Hero = () => {
-  return <div>Hero</div>;
+import Typed from "../Typed";
+
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] -mt-24 w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="text-4xl sm:text-7xl font-bold sm:py-6">
+          Grow with data.
+        </h1>
+        <div>
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Fast, flexible financing for{" "}
+            <Typed textArray={["BTB", "BTC", "SASS"]} />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Hero;
