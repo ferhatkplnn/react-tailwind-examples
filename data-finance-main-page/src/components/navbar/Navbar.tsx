43c3108a8391fd094f4cdbb10a198d685fba9341
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-17 00:08:01 +0300

    refactor: Refactor Navbar component for improved readability and maintainability
    - Reorganized code structure to use map function for menu items
    - Defined a separate function for toggling the mobile menu
    - Enhanced code clarity and maintainability

--- data-finance-main-page/src/components/navbar/Navbar.tsx
+++ data-finance-main-page/src/components/navbar/Navbar.tsx
@@ -7,35 +7,38 @@ import NavbarLinkItem from "./NavbarLinkItem";
 const Navbar = () => {
   const [isOpenMenuBar, setIsOpenMenuBar] = useState(false);
 
+  const toggleMenuBar = () => {
+    setIsOpenMenuBar(!isOpenMenuBar);
+  };
+
+  const menuItems = ["Home", "Company", "Resources", "About", "Contact"];
+
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">React.</h1>
+
+      {/* Desktop navigation */}
       <ul className="hidden sm:flex">
-        {["Home", "Company", "Resources", "About", "Contact"].map((item) => (
-          <NavbarLinkItem text={item} />
+        {menuItems.map((item, index) => (
+          <NavbarLinkItem text={item} key={index} />
         ))}
       </ul>
-      <div
-        onClick={() => setIsOpenMenuBar(!isOpenMenuBar)}
-        className="sm:hidden"
-      >
-        {isOpenMenuBar ? (
-          <MdClose size={24} className="cursor-pointer" />
-        ) : (
-          <AiOutlineMenu size={20} className="cursor-pointer" />
-        )}
+
+      {/* Mobile menu toggle */}
+      <div onClick={toggleMenuBar} className="sm:hidden cursor-pointer">
+        {isOpenMenuBar ? <MdClose size={24} /> : <AiOutlineMenu size={20} />}
       </div>
 
       {/* SideNavMenu for mobile */}
       <div
-        className={`fixed w-3/5 h-full pt-4  bg-zinc-950 left-0 top-0 ${
+        className={`fixed w-3/5 h-full pt-4 bg-zinc-950 left-0 top-0 transition-transform duration-500 ease-in ${
           isOpenMenuBar ? "translate-x-0" : "-translate-x-full"
-        } duration-500 ease-in`}
+        }`}
       >
         <h1 className="w-full text-3xl font-bold m-4 text-[#00df9a]">React.</h1>
-        <ul className="uppercase p-2 pt-4 ">
-          {["Home", "Company", "Resources", "About", "Contact"].map((item) => (
-            <MenubarItem text={item} />
+        <ul className="uppercase p-2 pt-4">
+          {menuItems.map((item, index) => (
+            <MenubarItem text={item} key={index} />
           ))}
         </ul>
       </div>
