cc403743000c2a7f9f1ed4c8b7be381629d243b3
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-17 00:05:56 +0300

    style: Enhance font weight for NavbarLinkItem
    - Updated font weight to font-semibold for better visibility and emphasis

--- data-finance-main-page/src/components/navbar/NavbarLinkItem.tsx
+++ data-finance-main-page/src/components/navbar/NavbarLinkItem.tsx
@@ -1,7 +1,7 @@
 const NavbarLinkItem = ({ text }: { text: string }) => {
   return (
     <div className="group">
-      <li className="p-4 pb-1 cursor-pointer">
+      <li className="p-4 pb-1 font-semibold cursor-pointer">
         {text}
         <div className="bg-white h-0.5 w-0 group-hover:w-full  duration-500 ease-out"></div>
       </li>
