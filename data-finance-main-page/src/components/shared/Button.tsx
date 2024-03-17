e85e87fc7f4c97cb219a1f61c28e1e8c14cb295c
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-17 13:18:30 +0300

    feat: Add Button component with customizable text and styles
    - Created a new Button component to encapsulate button functionality
    - Accepts text prop for customizable button text
    - Applies default styles and hover effect for better user interaction

---
+++ data-finance-main-page/src/components/shared/Button.tsx
@@ -0,0 +1,18 @@
+import { ComponentPropsWithoutRef } from "react";
+
+type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  text: string;
+};
+
+const Button = ({ text, className, ...props }: ButtonProps) => {
+  return (
+    <button
+      className={`${className} bg-white text-black font-bold py-3 px-10 rounded-lg hover:scale-105 duration-300 `}
+      {...props}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
