53ed1fa9abda07c7c210b486da4ee39cbaf423f1
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-17 13:19:24 +0300

    feat: Add Get Started button to Hero section
    - Added a new button to prompt user action in the Hero section
    - Updated text layout and added explanatory message for better clarity

--- data-finance-main-page/src/components/hero/Hero.tsx
+++ data-finance-main-page/src/components/hero/Hero.tsx
@@ -1,20 +1,27 @@
 import Typed from "../Typed";
+import Button from "../shared/Button";
 
 const Hero = () => {
   return (
     <div className="text-white">
-      <div className="max-w-[800px] -mt-24 w-full h-screen mx-auto text-center flex flex-col justify-center">
+      <div className="max-w-[800px] -mt-24 w-full h-screen mx-auto text-center flex flex-col justify-center space-y-2">
         <p className="text-[#00df9a] font-bold p-2">
           GROWING WITH DATA ANALYTICS
         </p>
         <h1 className="text-4xl sm:text-7xl font-bold sm:py-6">
           Grow with data.
         </h1>
+
+        <div className="md:text-5xl sm:text-4xl text-xl font-bold sm:py-4">
+          Fast, flexible financing for{" "}
+          <Typed textArray={["BTB", "BTC", "SASS"]} />
+        </div>
+        <p className="sm:text-2xl text-xl font-medium text-gray-500">
+          Monitor your data analytics to increase revenues for BTB, BTC & SASS
+          platforms.
+        </p>
         <div>
-          <div className="md:text-5xl sm:text-4xl text-xl font-bold">
-            Fast, flexible financing for{" "}
-            <Typed textArray={["BTB", "BTC", "SASS"]} />
-          </div>
+          <Button text="Get Started" className="bg-[#00df9a] w-48" />
         </div>
       </div>
     </div>
